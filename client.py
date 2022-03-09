05a36939a8dc66f532470f146df4225bc7308028
Oleg <zelo78@yandex.ru>
2022-03-10 02:42:48 +0300

    Add remote read and write to DB

--- client.py
+++ client.py
@@ -1,30 +1,82 @@
 import argparse
-from urllib import request, parse
+import http.client
+import json
 
-HOST, PORT = "http://localhost", 8000
+# from urllib import request, parse
+
+HOST, PORT = "localhost", 8000
 
 
 def main():
     args = command_line_processing()
+    args.func(args)
 
-    if args.command == 'task':
-        add_task(args)
-    else:
-        print(args)
+
+def ask_server(method: str, data: dict) -> dict:
+    """Соединяется с сервером методом `method`, передаём в теле запроса данные `data`.
+    Читаем ответ, и возвращаем его в словаре
+     {'status': код ответа сервера, 'reason': ответ сервера,
+     'data': возвращенные данные}"""
+
+    assert method in ['POST', 'GET']
+
+    connection = http.client.HTTPConnection(host=HOST, port=PORT)
+    connection.request(
+        method=method,
+        url='/api/v1/task',
+        headers={'Content-type': 'application/json'},
+        body=json.dumps(data),
+    )
+    response = connection.getresponse()
+    try:
+        received_data = json.loads(response.read().decode('utf-8'))
+    except json.JSONDecodeError:
+        received_data = None
+
+    result = {
+        'status': response.status,
+        'reason': response.reason,
+        'data': received_data,
+    }
+    connection.close()
+    return result
 
 
 def add_task(args):
+    # Подготовка данных
     data = {
         'task_type': args.task_type,
         'string': args.string,
     }
-    req = request.Request(
-        url=f'{HOST}:{PORT}',
-        data=parse.urlencode(data).encode('utf-8'),
-        method='POST',
-    )
-    with request.urlopen(url=req) as f:
-        print(f)
+    result = ask_server('POST', data)
+
+    if result['status'] == http.client.CREATED:
+        identifier = result['data']['id']
+        print(f'Создана новая задача с идентификатором {identifier}.')
+    else:
+        print(f'Ошибка, код ошибки {result["status"]}, описание ошибки {result["reason"]}.')
+
+
+def show_task(args):
+    # Подготовка данных
+    identifier = args.id
+    data = {'id': identifier}
+    result = ask_server('GET', data)
+
+    if result['status'] == http.client.OK:
+        if args.command == 'status':
+            status = result['data']['status']
+            print(f'Задача с идентификатором {identifier} имеет статус {status}.')
+        elif args.command == 'result':
+            res = result['data']['result']
+            if res is None:
+                print(f'Задача с идентификатором {identifier} пока не завершена.')
+            else:
+                print(f'Задача с идентификатором {identifier} получила результат {res}.')
+    elif result['status'] == http.client.NOT_FOUND:
+        print(f'Задача с идентификатором {identifier} в базе не найдена.')
+    else:
+        print(f'Ошибка, код ошибки {result["status"]}, описание ошибки {result["reason"]}.')
 
 
 def command_line_processing():
@@ -56,6 +108,7 @@ def command_line_processing():
         'string',
         help='Исходная строка',
         )
+    parser_task.set_defaults(func=add_task)
     
     parser_status = subparsers.add_parser(
         'status', 
@@ -67,6 +120,7 @@ def command_line_processing():
         type=int,
         help='Идентификатор задачи',
         )
+    parser_status.set_defaults(func=show_task)
 
     parser_result = subparsers.add_parser(
         'result', 
@@ -78,6 +132,7 @@ def command_line_processing():
         type=int,
         help='Идентификатор задачи',
         )
+    parser_result.set_defaults(func=show_task)
 
     parser_wait = subparsers.add_parser(
         'wait', 
@@ -94,12 +149,14 @@ def command_line_processing():
         'source',
         help='Исходная строка',
         )
+    # parser_wait.set_defaults(func=wait_for_task)
 
     parser_stop = subparsers.add_parser(
         'stop', 
         help='Остановить сервер и процесс выполнения задач',
         description='Останавливает сервер и процесс выполнения задач',
         )
+    # parser_stop.set_defaults(func=stop_server)
     
     return parser.parse_args()
 
--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import http.client
+import json
+from threading import Lock
 
 
 DB = []
+db_lock = Lock()
 
 
 class Task:
@@ -11,22 +15,50 @@ class Task:
         self.state = 'pending'
         self.result = None
 
+    def __str__(self):
+        return f'Task: type {self.type_code}, string {self.string}, state {self.state}, result {self.result}'
+
 
 class RequestHandler(BaseHTTPRequestHandler):
 
-    # определяем метод `do_GET`
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
-
-    def do_POST(self):
+    def do_GET(self):  # noqa
         content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        print(body)
-        self.send_response(200)
+        data = json.loads(self.rfile.read(content_length))
+        identifier = data['id']
+        with db_lock:
+            if 0 <= identifier < len(DB):
+                task = DB[identifier]
+            else:
+                task = None
+
+        if task is None:
+            self.send_response(http.client.NOT_FOUND)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+        else:
+            data = {
+                'status': task.state,
+                'result' : task.result,
+            }
+            self.send_response(http.client.OK)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+    def do_POST(self):  # noqa
+        content_length = int(self.headers['Content-Length'])
+        data = json.loads(self.rfile.read(content_length))
+        task = Task(type_code=data['task_type'], string=data['string'])
+        with db_lock:
+            DB.append(task)
+            identifier = len(DB) - 1
+
+        print(f'{task} was added as #{identifier}')
+        data = {'id': identifier}
+        self.send_response(http.client.CREATED)
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+        self.wfile.write(json.dumps(data).encode('utf-8'))
 
 
 httpd = HTTPServer(('localhost', 8000), RequestHandler)
